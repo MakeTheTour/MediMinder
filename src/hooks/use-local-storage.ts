b28adedfdb71111cb1e40406ce0d71fcab60f3b0
Asraful Islam <asrafulblogs@gmail.com>
2025-08-11 18:48:51 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/hooks/use-local-storage.ts
+++ src/hooks/use-local-storage.ts
@@ -19,10 +19,6 @@ function getInitialValue<T>(key: string, initialValue: T) {
 export function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T | ((val: T) => T)) => void] {
   const [storedValue, setStoredValue] = useState<T>(() => getInitialValue(key, initialValue));
 
-  useEffect(() => {
-    setStoredValue(() => getInitialValue(key, initialValue));
-  }, [key, initialValue]);
-
   const setValue = useCallback((value: T | ((val: T) => T)) => {
     if (typeof window === 'undefined') {
       console.warn(`Tried to set localStorage key "${key}" even though no window was found`);
@@ -30,13 +26,13 @@ export function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T 
     }
 
     try {
-      const valueToStore = value instanceof Function ? value(storedValue) : value;
+      const valueToStore = value instanceof Function ? value(getInitialValue(key, initialValue)) : value;
       setStoredValue(valueToStore);
       window.localStorage.setItem(key, JSON.stringify(valueToStore));
     } catch (error) {
       console.error(error);
     }
-  }, [key, storedValue]);
+  }, [key, initialValue]);
 
   return [storedValue, setValue];
 }
