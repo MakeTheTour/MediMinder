66705a4587dbeb39085e4b0ff3d1d664809f4607
Asraful Islam <asrafulblogs@gmail.com>
2025-08-14 03:57:34 +0000

    Same error. Firebase: error (auth-configuration not found) why show this

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBnSjiOBYUbovMxm3yMCD8O8ol8ibr9Y-0",
-  authDomain: "mediminder-50jgi.firebaseapp.com",
-  projectId: "mediminder-50jgi",
-  storageBucket: "mediminder-50jgi.firebasestorage.app",
-  messagingSenderId: "811154320643",
-  appId: "1:811154320643:web:42107016d08f6bcc568d36",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 
