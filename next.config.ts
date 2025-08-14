bced57d2ec22b9a3542f9bfe76e34a192bb9bb65
Asraful Islam <asrafulblogs@gmail.com>
2025-08-14 03:44:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -23,16 +23,6 @@ const nextConfig: NextConfig = {
       'https://*.cloudworkstations.dev',
     ],
   },
-  publicRuntimeConfig: {
-    firebase: {
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    }
-  }
 };
 
 export default nextConfig;
